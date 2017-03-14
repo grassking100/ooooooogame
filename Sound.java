f0bbce34d0ceae9afe278362b4f2322730f14896
123@hot.com <123@hot.com>
2017-03-14 17:34:18 +0800

    type something

--- Sound.java
+++ Sound.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class A01 {
+public class Sound{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
